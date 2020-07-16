3e6132587b51ba036dbc732d869ffa73486b89b6
SantiagoAlvarezz <67876948+SantiagoAlvarezz@users.noreply.github.com>
2020-07-16 08:45:13 -0500

    Add files via upload

---
+++ EcuacionCuadratica.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{//ingreso de variables a, b, c
+		Console.WriteLine("Ingrese el valor de a: ");
+		double a = double.Parse(Console.ReadLine());
+
+		Console.WriteLine("Ingrese el valor de b: ");
+		double b = double.Parse(Console.ReadLine());
+
+		Console.WriteLine("Ingrese el valor de c: ");
+		double c = double.Parse(Console.ReadLine());
+		//ecuaciones una con raiz Negativa, la otra con raiz positiva
+		double raizPositiva = (-b + (Math.Sqrt(Math.Pow(b, 2) - 4 * a * c))) / (2 * a);
+		double raizNegativa = (-b - (Math.Sqrt(Math.Pow(b, 2) - 4 * a * c))) / (2 * a);
+		// que me muestre la solucion a ambas raices
+		Console.WriteLine("x1: " + raizPositiva);
+		Console.WriteLine("x2: " + raizNegativa);
+		//ni me acuerdo que hice aquí, si las raices dan nros imaginarios/raros, no sé
+		if (raizNegativa.ToString() == "NaN" || raizPositiva.ToString() == "NaN")
+		{
+			Console.WriteLine("No es posible calcular la solucion. Tas bien(?).");
+
+		}
+		else if (raizPositiva != raizNegativa)
+		{// Ambas son calculabes y correctas 
+			Console.WriteLine("Ambas soluciones son correctas :D");
+
+
+
+		}
+		else
+		{//tiene unica solución
+			Console.WriteLine("Unica Solución");
+
+		}
+
+	}
+}
---
+++ IndiceMasaCorporal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{//Ingreso de datos, el peso EN KG y la altura EN METROS
+		Console.WriteLine("Ingrese su peso en kg: ");
+		double kg = double.Parse(Console.ReadLine());
+
+		Console.WriteLine("Ingresa su altura en metros: ");
+		double m = double.Parse(Console.ReadLine());
+		//Elevo la altura al cuadrado por que mas adelante lo necesitaremos para operar xd
+		double mCuadrado = m * m;
+		//ecuacion del imc
+		double IMC = kg / mCuadrado;
+
+		Console.WriteLine("Su indicie de masa corporal es:" + IMC);
+		//si la ecuacion anterior es menor o igual a eso
+		if (IMC <= 18.5)
+		{
+			Console.WriteLine("Estás bajo de peso :(");
+		}
+		//si esta entre ese rango GgEz
+		else if (18.6 <= IMC && IMC <= 24.9)
+		{
+			Console.WriteLine("Tu peso es normal :D");
+		}
+		else if (25 <= IMC && IMC <= 29.9)
+		{
+			Console.WriteLine("Tienes sobrepeso D:");
+		}
+		else
+		//De lo contrario
+		{
+			Console.WriteLine("Tienes obesidad :(");
+
+		}
+
+	}
+}

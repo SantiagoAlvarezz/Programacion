f6d1851ac13e6f76ac17e997a3ac455b1a31e4b7
SantiagoAlvarezz <67876948+SantiagoAlvarezz@users.noreply.github.com>
2020-08-13 10:02:31 -0500

    Add files via upload

---
+++ Captcha.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{//declaracion de variablessssss
+		Random aleatorio = new Random();
+		int a = 0, b = 0, sumacorrecta = 0, intentos = 3, respuesta = 0;
+		//Inicio ciclooo
+		while (intentos > 0)
+		{
+			a = aleatorio.Next(0, 11);
+			b = aleatorio.Next(0, 11);
+			sumacorrecta = a + b;
+			//operacion de captcha
+			Console.WriteLine("Sume " + a + "+" + b);
+			// empiezan las condiciones
+			respuesta = int.Parse(Console.ReadLine());
+			if (respuesta == sumacorrecta)
+			{
+				Console.WriteLine("Puede continuar");
+				break;
+
+			}
+			else
+			{
+				intentos--;
+				Console.WriteLine("Intente nuevamente");
+
+			}
+		}
+		if (intentos == 0)
+		{
+			Console.WriteLine("Validación incorrecta, no puede continuar.");
+		}
+
+	}
+}

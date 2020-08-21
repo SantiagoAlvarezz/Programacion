6c2fe6fec4ac2dcd22692bcef26ef5032421ae23
SantiagoAlvarezz <67876948+SantiagoAlvarezz@users.noreply.github.com>
2020-08-21 03:22:18 -0500

    Add files via upload

---
+++ Euler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double n = 100, x = 3, factorial = 0, Total = 0;
+        for (int i = 0; i <= n; i++)
+        {
+            factorial *= i;
+            if (factorial == 0) factorial = 1;
+            Total += (Math.Pow(x, i)) / (factorial);
+
+        }
+        Console.WriteLine(Total);
+    }
+}
---
+++ SenoRaro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese x:");
+        double x = double.Parse(Console.ReadLine());
+        double Total = 0;
+        int n = 100
+
+
+        for (int i = 0; i <= n; i++)
+        {
+            double tmp = (2 * 1) + 1;
+            Total += ((Math.Pow(-1, i))/ factorial(tmp)) * Math.Pow(x, tmp) 
+        }
+        Console.WriteLine("sen(" + x + ") =" + Total);
+    }
+    static double factorial(double valor)
+    {
+        if (valor <= 1) return (2 * 1) + 1;
+        return valor * factorial(valor - 1)//jaja esta parte esta mala xd
+    }
+}
